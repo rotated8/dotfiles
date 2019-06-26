4bee2220405c5ff5f7b697b7a55cc73b35fb3bc2
Collin Brittle <cnbrittle@gmail.com>
2019-06-26 18:54:52 -0400

    Update Vagrantfile and setup script for Ubuntu 19.04
    Plus, add more documentation

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,23 @@
+Vagrant.require_version(">= 1.8") # Require Vagrant 1.8 at a minimum
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.box = "bento/ubuntu-19.04"
 
+  # The host_ip is a workaround for Windows hosts, which otherwise do not like to forward ports.
   config.vm.network "forwarded_port", guest: 8983, host_ip: "127.0.0.1", host: 8983, auto_correct: true
   config.vm.network "forwarded_port", guest: 8984, host_ip: "127.0.0.1", host: 8984, auto_correct: true
   config.vm.network "forwarded_port", guest: 3000, host_ip: "127.0.0.1", host: 3000, auto_correct: true
 
   config.vm.provider "virtualbox" do |vb|
-    vb.cpus   = 2
-    vb.memory = 2048
+    #vb.name         = "my box"
+    vb.cpus         = 2
+    vb.memory       = 2048
+    vb.linked_clone = true # Linked clones prevents multiple VMs from being created for the same vagrant base box.
   end
 
   config.vm.provision "shell" do |s|
     s.env        = { REPO_ORG: "", REPO_NAME: "" }
     s.path       = "./setup-vagrant.sh"
-    s.reset      = true
+    s.reset      = true # Reloads the shell after the provisioner runs
     s.privileged = false
   end
 end
--- setup-vagrant.sh
+++ setup-vagrant.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
-# Exit at first failure, return failures from pipes, and don't allow unset vars
-set -o errexit -o pipefail -o nounset
+# Exit at first failure, return failures from pipes, don't allow unset vars, and echo executed lines in output.
+set -o errexit -o pipefail -o nounset -o xtrace
 
-### For use with Ubuntu 16.04 or later.
+# For use with Ubuntu 19.04 or later.
 
 # Install updates and basic packages
+export DEBIAN_FRONTEND=noninteractive # Because it's true, and it prevents some dpkg-*configure errors.
 sudo apt-get update
-sudo apt-get install -y git vim make gcc g++ software-properties-common
+sudo apt-get install -y git vim make gcc g++ software-properties-common neovim ripgrep
 
-# Install Neovim
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install -y neovim
+# Install Neovim on Ubuntu pre-18.04
+#sudo add-apt-repository -y ppa:neovim-ppa/stable
+#sudo apt-get update
+#sudo apt-get install -y neovim
+
+# Install ripgrep on Ubuntu pre-18.10
+#curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
+#sudo dpkg -i ./ripgrep_11.0.1_amd64.deb
 
 # Install Rbenv, ruby-build
 git clone https://github.com/rbenv/rbenv.git "${HOME}/.rbenv"
@@ -26,7 +31,9 @@ git clone https://github.com/rotated8/dotfiles "${dot_files_dir}"
 
 # Source the .bashrc, if it exists, to get rbenv working.
 if [[ -e "${HOME}/.bashrc" ]]; then
+    set +o xtrace # Please don't trace this.
     . "${HOME}/.bashrc"
+    set -o xtrace
 fi
 
 # Install Ruby 2.5.3
@@ -34,16 +41,15 @@ rbenv install 2.5.3
 rbenv global 2.5.3
 
 # Update system gems, and force bundler to get installed.
-gem update --system
-gem install --force bundler
+gem update --system --no-document --no-post-install-message
+gem install --force --no-document bundler
 
 # Install package dependencies
-sudo apt-get install -y libmysqlclient-dev libsqlite3-dev libpq-dev
-sudo apt-get install -y redis-server postgresql chromium-browser chromium-chromedriver openjdk-8-jre
+sudo apt-get install -y redis-server chromium-browser chromium-chromedriver openjdk-8-jre libmysqlclient-dev libsqlite3-dev #libpq-dev postgresql
 
 # Install Node 8
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install nodejs
+sudo apt-get install -y nodejs
 
 # Install Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -60,5 +66,5 @@ if [[ -n "${REPO_ORG}" && -n "${REPO_NAME}" ]]; then
 
     # Install gems
     cd "${repo_dir}"
-    bundle install
+    bundle install --jobs 4 --retry 3
 fi
