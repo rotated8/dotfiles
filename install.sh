09d1f144595baaf199c3906fb7a91671be73a3c2
Collin Brittle <cnbrittle@gmail.com>
2023-03-14 03:07:53 -0400

    Cleans up install.sh
    Makes `git clone` quiet, and logs work as it is done.

--- install.sh
+++ install.sh
@@ -11,10 +11,17 @@ else
     echo "Could not find a directory to copy the dotfiles from. Try passing a path to this script."
     exit 1
 fi
+echo "Using dotfiles from $dot_file_dir"
 
 # Copy files
 for dotfile in ".vimrc" ".bashrc" ".inputrc" ".gitconfig" ".tmux.conf" "git-completion.bash"; do
+    if [[ -f "${HOME}/${dotfile}" ]]; then
+        echo "${HOME}/${dotfile} already exists, backing it up."
+        cp "${HOME}/${dotfile}" "${HOME}/${dotfile}.backup"
+    fi
+
     if [[ -f "${dot_file_dir}/${dotfile}" ]]; then
+        echo "Installing ${dotfile}..."
         cp "${dot_file_dir}/${dotfile}" "${HOME}/${dotfile}"
     else
         echo "File ${dotfile} not found in ${dot_file_dir}. Skipping..."
@@ -24,16 +31,18 @@ done
 # Set up Vim
 if [[ -e "${HOME}/.vimrc" ]]; then
     # Install Vundle if it is missing.
+    echo "Installing Vim plugins..."
     if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
-        git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+        git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
     fi
 
     # Install plugins
     # -E = Start in Ex mode. -s = Silent. -S = Source this file (since -E won't do it)
     vim -EsS "${HOME}/.vimrc" +PluginInstall +qall 1> /dev/null 2> /dev/null
 
-    # Set up nvim if installed. https://neovim.io/
+    # Set up nvim if installed.
     if which nvim 1> /dev/null 2> /dev/null; then
+        echo "Installing Neovim plugins..."
         # Link the vimrc, if it is missing
         if [[ ! -e "${HOME}/.config/nvim/init.vim" ]]; then
             mkdir -p "${HOME}/.config/nvim/"
@@ -42,7 +51,7 @@ if [[ -e "${HOME}/.vimrc" ]]; then
 
         # Keep Vundle installs separate, in case something conflicts
         if [[ ! -d "${HOME}/.config/nvim/bundle/Vundle.vim" ]]; then
-            git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.config/nvim/bundle/Vundle.vim"
+            git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${HOME}/.config/nvim/bundle/Vundle.vim"
         fi
 
         # Plugins still need to be installed
@@ -50,5 +59,5 @@ if [[ -e "${HOME}/.vimrc" ]]; then
     fi
 
 else
-    echo "Missing .vimrc"
+    echo "No .vimrc, skipping Vim/Neovim setup."
 fi
