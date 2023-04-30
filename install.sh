83767cda567f952314e8a67b5ba39207354f4317
Collin Brittle <cnbrittle@gmail.com>
2023-04-30 02:01:59 -0400

    Add Lua plugins for Neovim and install init.lua if possible
    Honestly, the install.sh changes were the worse to make. Redoing all the
    plugin setup was a pain, but at  least I had documentation to work with.
    I don't know if I actually like lazy.nvim as a package manager yet, but
    everyone seems to swear by it.
    
    The Lua plugins are mostly replacements for Vimscript equivalents. All
    found via https://github.com/rockerBOO/awesome-neovim

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo "Using dotfiles from $dot_file_dir"
 # Copy files
 for dotfile in ".vimrc" ".bashrc" ".inputrc" ".gitconfig" ".tmux.conf" "git-completion.bash"; do
     if [[ -f "${HOME}/${dotfile}" ]]; then
-        echo "${HOME}/${dotfile} already exists, backing it up."
+        echo "${HOME}/${dotfile} already exists, backing it up to ${HOME}."
         cp "${HOME}/${dotfile}" "${HOME}/${dotfile}.backup"
     fi
 
@@ -28,36 +28,65 @@ for dotfile in ".vimrc" ".bashrc" ".inputrc" ".gitconfig" ".tmux.conf" "git-comp
     fi
 done
 
-# Set up Vim
-if [[ -e "${HOME}/.vimrc" ]]; then
-    # Install Vundle if it is missing.
-    echo "Installing Vim plugins..."
-    if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
-        git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
-    fi
+# Neovim Setup
+if [[ -n "`command -v nvim`" ]]; then
+    echo "Attempting to configure Neovim..."
 
-    # Install plugins
-    # -E = Start in Ex mode. -s = Silent. -S = Source this file (since -E won't do it)
-    vim -EsS "${HOME}/.vimrc" +PluginInstall +qall 1> /dev/null 2> /dev/null
+    nvim_config_dir="${XDG_CONFIG_HOME:-$HOME}/.config/nvim"
+    mkdir -p "${nvim_config_dir}/"
 
-    # Set up nvim if installed.
-    if which nvim 1> /dev/null 2> /dev/null; then
-        echo "Installing Neovim plugins..."
-        # Link the vimrc, if it is missing
-        if [[ ! -e "${HOME}/.config/nvim/init.vim" ]]; then
-            mkdir -p "${HOME}/.config/nvim/"
-            ln "${HOME}/.vimrc" "${HOME}/.config/nvim/init.vim"
+    # Back up init files
+    for init in "init.lua" "init.vim" ; do
+        if [[ -f "${nvim_config_dir}/${init}" ]]; then
+            echo "${nvim_config_dir}/${init} already exists, backing it up to ${HOME}."
+            cp "${nvim_config_dir}/${init}" "${HOME}/${init}.backup"
+        fi
+    done
+
+    # Install init.lua, if it exists
+    if [[ -f "${dot_file_dir}/init.lua" ]]; then
+        echo "Installing init.lua..."
+        cp "${dot_file_dir}/init.lua" "${nvim_config_dir}/init.lua"
+
+        # Avoid attempting to use init.lua and init.vim.
+        if [[ -f "${nvim_config_dir}/init.vim" ]]; then
+            echo "Removing ${nvim_config_dir}/init.vim, since init.lua has been installed."
+            rm "${nvim_config_dir}/init.vim"
+            echo "Plugins installed by init.vim must be cleaned up manually."
         fi
 
-        # Keep Vundle installs separate, in case something conflicts
+        # Install plugins
+        nvim --headless "+Lazy! sync" +qall 1> /dev/null 2>dev/null
+    elif [[ -e "${HOME}/.vimrc" ]]; then
+        # Install init.vim if init.lua is missing
+        echo "Linking .vimrc as init.vim..."
+        ln "${HOME}/.vimrc" "${nvim_config_dir}/init.vim"
+
+        # Install plugins
         if [[ ! -d "${HOME}/.config/nvim/bundle/Vundle.vim" ]]; then
             git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${HOME}/.config/nvim/bundle/Vundle.vim"
         fi
-
-        # Plugins still need to be installed
         nvim --headless +PluginInstall +qall 1> /dev/null 2> /dev/null
+    else
+        echo "Neovim is installed, but no configuration file was found, moving on..."
     fi
-
 else
-    echo "No .vimrc, skipping Vim/Neovim setup."
+    echo "Neovim is missing, moving on..."
+
+    # Neovim is missing. Only set up Vim if it is present with a .vimrc
+    if [[ -n "`command -v vim`" && -e "${HOME}/.vimrc" ]]
+        # Install Vundle if it is missing.
+        echo "Installing Vim plugins..."
+        if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
+            git clone --quiet https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+        fi
+
+        # Install plugins
+        # -E = Start in Ex mode. -s = Silent. -S = Source this file (since -E won't do it)
+        vim -EsS "${HOME}/.vimrc" +PluginInstall +qall 1> /dev/null 2> /dev/null
+    else
+        echo "Vim or a .vimrc are missing, moving on..."
+    fi
 fi
+
+echo "Successfully finished installation!"
