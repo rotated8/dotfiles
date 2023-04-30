70f88833a5790bd1357283fd5558421c3c4d40da
Collin Brittle <cnbrittle@gmail.com>
2023-04-30 02:19:48 -0400

    Add saftey to the Neovim benchmarking script
    At least check if Hyperfine and Neovim are available before going crazy

--- vim-compare/benchmark.sh
+++ vim-compare/benchmark.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# Expect hyperfine and neovim to already be installed. Neovim should have plugins already installed as well.
+if [[ -z "`command -v nvim`" && -z "`command -v hyperfine`" ]]; then
+    echo "Install Hyperfine and Neovim, and any plugins Neovim needs."
+    exit 1
+fi
+
 hyperfine "nvim -u NORC +qa" --warmup=20
 find . -maxdepth 1 -regextype egrep -regex ".*(vim|lua)" | sort | xargs -I{} hyperfine "nvim -u '{}' +qa" --warmup=20
