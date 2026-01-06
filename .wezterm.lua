dd491bdb2227dbbc4e829096b17e7c1cd484fde5
Collin Brittle <cnbrittle@gmail.com>
2026-01-06 14:09:29 -0500

    Rewrites WezTerm config
    My understanding has improved, and so has the config. WSL is
    automatically configured, so this sets it as the default. It also
    changes the default 'local' command to PowerShell. Then, it adds back a
    bunch of copy/paste stuff that I rely on from the MS Terminal- <C-C> for
    copy, <C-V> for paste, and right click appropriate copy/paste as well.
    Finally, it'll try to find a monitor in portrait orientation and full
    screen the window there on startup. It'll still default to the main
    monitor if no portrait one is found.

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,20 +3,45 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Zenburn'
 config.font = wezterm.font 'Iosevka Term'
-config.default_prog = { 'wsl.exe' }
-config.launch_menu = {
-    {
-        label = 'Ubuntu',
-        args = { 'wsl.exe', '-d', 'Ubuntu' }
-    },
-    {
-        label = 'PowerShell',
-        args = { 'pwsh.exe' }
-    },
-    {
-        label = 'cmd',
-        args = { 'cmd.exe'}
-    }
+config.default_prog = { 'pwsh.exe' }
+config.default_domain = 'WSL:Ubuntu'
+config.keys = {
+    { key = 'c', mods = 'CTRL', action = wezterm.action.CopyTo 'Clipboard' },
+    { key = 'v', mods = 'CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
 }
+-- Right-mouse copy and paste, via https://github.com/wezterm/wezterm/discussions/3541#discussioncomment-5633570
+config.mouse_bindings = {
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+            if has_selection then
+                window:perform_action(wezterm.action.CopyTo("ClipboardAndPrimarySelection"), pane)
+                window:perform_action(wezterm.action.ClearSelection, pane)
+            else
+                window:perform_action(wezterm.action({ PasteFrom = "Clipboard" }), pane)
+            end
+        end),
+    },
+}
+
+wezterm.on('gui-startup', function(cmd)
+    -- I use a vertical monitor sometimes. Find it, or default to the main one.
+    local target_screen = wezterm.gui.screens().main.name
+    for screen_name, screen in pairs(wezterm.gui.screens().by_name) do
+        if screen.height > screen.width then
+            target_screen = screen_name
+        end
+    end
+
+    -- Edit the command, if any, to move the window to the target screen
+    local cmd = cmd or {}
+    cmd.position = { ['x']=1, ['y']=1, ['origin']={ ['Named']=target_screen } }
+
+    -- Maximize the window.
+    local tab, pane, window = wezterm.mux.spawn_window(cmd)
+    window:gui_window():maximize()
+end)
 
 return config
